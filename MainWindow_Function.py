cc3c6407352260b156b999028272301ff32017cc
xiangyubo <admin@paint4d.com>
2023-07-14 10:50:42 +0800

    write hand font deep learning test

--- MainWindow_Function.py
+++ MainWindow_Function.py
@@ -17,23 +17,21 @@ from PyQt5.QtCore import QTimer, QRect
 
 
 class MainWindowControl(object):
-    widget = pyqt.GraphicsLayoutWidget()
-    plot_window = None
-    plot_window02 = None
-    time = QTimer()
-    plot_item = pyqt.PlotDataItem()
-    img_item = pyqt.ImageItem()
 
     def __init__(self):
-        self.plot_window = self.widget.addPlot(tilte='Show')
-        self.plot_window02 = self.widget.addPlot(tilte='S')
-        self.plot_window.showGrid(x=True, y=True)
-        self.plot_window02.showGrid(x=True, y=True)
-        self.plot_window02.setLabel('left', "Coss", units='0-1')
-        self.plot_window02.setLabel('bottom', "Iteration", units='times')
+        self.model = XYBDeepLearn.XYBDeepModel()
+        self.op = tr.optim.Adam(self.model.parameters(), lr=0.001)
+        self.data = XYBDeepLearn.XYBDataLoader(self.model.device_id)
 
-        self.img_item = pyqt.ImageItem(image=np.zeros((64, 64, 1)))
-        self.img_item.setRect(QRect(-1, -1, 2, 2))
+        self.widget = pyqt.GraphicsLayoutWidget()
+        self.plot_window = self.widget.addPlot(tilte='Preview')
+        self.plot_window.setAspectLocked(True)
+        self.loss_window = self.widget.addPlot(tilte='Loss')
+        self.loss_window.setAspectLocked(True)
+        self.plot_window.showGrid(x=True, y=True)
+        self.loss_window.showGrid(x=True, y=True)
+        self.loss_window.setLabel('left', "Coss", units='0-1')
+        self.loss_window.setLabel('bottom', "Iteration", units='times')
 
         self.x = [0, 1]
         self.y = [0, 1]
@@ -43,11 +41,9 @@ class MainWindowControl(object):
             hoverSize=5,
             hoverBrush=(255, 255, 255)
         )
-
-        self.plot_window.addItem(self.img_item)
         self.plot_window.addItem(self.plot_item)
 
-        self.loop_item = self.plot_window02.plot(
+        self.loop_item = self.loss_window.plot(
             pen=(0, 255, 0),
             filllevel=0.0,
             brush=(50, 50, 200, 200),
@@ -56,119 +52,121 @@ class MainWindowControl(object):
         self.time = QTimer()
         self.time.timeout.connect(self.update)
 
-        self.widget.nextRow()
-        self.plot_window03 = self.widget.addPlot(tilte='Gaussian Distribution')
+        self.time_paint = QTimer()
+        self.time_paint.timeout.connect(self.update_paint)
 
-    data_length = 20
+        self.loop_count = 0
+        self.coss = 0
+        self.loop_x = []
+        self.loop_y = []
+        self.test_text = []
 
-    def get_random_data(self, mini_batch: int):
-        r = np.random.permutation(range(0, self.data_length))
-        res = []
+        self.paint_img_text = pyqt.TextItem('Real Text Paint', color=(255, 0, 0))
+        self.paint_img_text.setPos(6, -6)
+        self.paint_img = pyqt.ImageItem(image=np.ones((XYBDeepLearn.ImageSize, XYBDeepLearn.ImageSize)) * 0)
+        self.paint_img.setRect(0, -6, 5, 5)
+        self.plot_window.addItem(self.paint_img)
+        self.plot_window.addItem(self.paint_img_text)
 
-        while len(r) > 0:
-            r_one = []
-            for i in range(0, mini_batch):
-                if len(r) == 0:
-                    break
-                r_one.append(r[0])
-                r = np.delete(r, [0, 0])
-            res.append(r_one)
+        kern = np.ones((3, 3))
+        self.paint_img.setDrawKernel(kern, mask=kern, center=(1, 1), mode='set')
+        self.paint_img.setLevels([0, 0])
 
-        return res
-
-    data_p = []
-    data_y = []
-    tr_res = []
-    model = XYBDeepLearn.XYBDeepModel()
-    op = tr.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+    def clear_canvas(self):
+        self.paint_img.setImage(image=np.ones((XYBDeepLearn.ImageSize, XYBDeepLearn.ImageSize)) * 0)
+        kern = np.ones((3, 3))
+        self.paint_img.setDrawKernel(kern, mask=kern, center=(1, 1), mode='set')
+        self.paint_img.setLevels([0, 0])
 
     def init_deeplearn(self):
-        self.op = tr.optim.SGD(self.model.parameters(), lr=0.1, momentum=0.4)
-        draw_x = []
-        draw_y = []
-        draw_color = []
-        self.data_p = []
-        self.data_y = []
-
-        for i in range(0, 10):
-            p = [random.random()*2-1, random.random()*2-1]
-            self.data_p.append(p)
-            self.data_y.append([0])
-
-            draw_x.append(p[0])
-            draw_y.append(p[1])
-            draw_color.append((255, 0, 0, 255))
-
-        for i in range(0, 10):
-            p = [random.random()*2-1, random.random()*2-1]
-            self.data_p.append(p)
-            self.data_y.append([1])
-
-            draw_x.append(p[0])
-            draw_y.append(p[1])
-            draw_color.append((0, 255, 0, 255))
-
-        self.plot_item.setData(x=draw_x, y=draw_y, brush=draw_color)
-
-        data_point_x = []
-        data_point_y = []
-        data_p = []
-        for x in range(0, 64):
-            for y in range(0, 64):
-                p = [-1 + x * (2 / 64), -1 + y * (2 / 64)]
-                data_point_x.append(p[0])
-                data_point_y.append(p[1])
-                data_p.append(p)
-        self.tr_res = tr.tensor(data_p, dtype=tr.float32)
         self.loop_x = []
         self.loop_y = []
         self.loop_count = 0
+        self.test_text = []
+        self.coss = 0
+
+        self.data.load_img('X:/PyDeepLearn/Train/9/', [0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        self.data.load_img('X:/PyDeepLearn/Train/8/', [0, 0, 0, 0, 0, 0, 0, 0, 1, 0])
+        self.data.load_img('X:/PyDeepLearn/Train/7/', [0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
+        self.data.load_img('X:/PyDeepLearn/Train/6/', [0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+        self.data.load_img('X:/PyDeepLearn/Train/5/', [0, 0, 0, 0, 0, 1, 0, 0, 0, 0])
+        self.data.load_img('X:/PyDeepLearn/Train/4/', [0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
+        self.data.load_img('X:/PyDeepLearn/Train/3/', [0, 0, 0, 1, 0, 0, 0, 0, 0, 0])
+        self.data.load_img('X:/PyDeepLearn/Train/2/', [0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+        self.data.load_img('X:/PyDeepLearn/Train/1/', [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.data.load_img('X:/PyDeepLearn/Train/0/', [1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+        self.data.load_test_img('X:/PyDeepLearn/Test/')
+        # Image Test DataReadLoader Show
+        x = 0
+        for img in self.data.test_list:
+            img_item = pyqt.ImageItem(
+                image=img.reshape(XYBDeepLearn.ImageSize, XYBDeepLearn.ImageSize).data.numpy()
+            )
+            img_item.setRect(QRect(x, 0, 1, 1))
+            self.plot_window.addItem(img_item)
+
+            text_item = pyqt.TextItem('Test', color=(0, 255, 0))
+            text_item.setParentItem(img_item)
+            text_item.setPos(x, 0)
+            self.test_text.append(text_item)
+            self.plot_window.addItem(text_item)
+            x += 1
 
     def start(self):
         print('start')
         self.time.start(24)
-
-    loop_count = 0
-    loop_item = pyqt.PlotDataItem()
-    loop_x = []
-    loop_y = []
+        self.time_paint.start(60)
 
     def update(self):
-        mini_batch = self.get_random_data(4)
-        coss = 0
+        data_batch = self.data.get_data_batch(300)
 
         self.model.train()
-        for batch in mini_batch:
-            self.op.zero_grad()
+        self.op.zero_grad()
 
-            res_x = []
-            res_y = []
-            for index in batch:
-                res_x.append(self.data_p[index])
-                res_y.append(self.data_y[index])
-            tr_x = tr.tensor(res_x, device=self.model.device_id)
-            tr_y = tr.tensor(res_y, device=self.model.device_id)
-            # summary(self.model, (len(batch), 2))
-            prob_y = self.model.forward(tr_x)
-            loss = tr.sum(-tr_y * tr.log(prob_y))
-            loss.backward(retain_graph=True)
-            self.op.step()
-            coss += (tr.sum(tr.abs(tr_y - prob_y))) / (prob_y.shape[0] * prob_y.shape[1])
+        x = data_batch[0]
+        y = data_batch[1]
 
-        self.loop_x.append(self.loop_count)
-        self.loop_y.append(coss.data.cpu().numpy().flatten()[0])
-        self.loop_item.setData(self.loop_x, self.loop_y)
+        prob_y = self.model.forward(x)
+        loss = tr.sum(-y * tr.log(prob_y)) / 300
 
-        self.model.eval()
-        res_data = self.model.forward(self.tr_res)
-        img_data = res_data.data.cpu().numpy().flatten().reshape((64, 64, 1))
-        self.img_item.setImage(img_data)
+        loss.backward(retain_graph=True)
+        self.coss += loss
+        self.op.step()
 
-        self.loop_count += 1
+        if data_batch[2] is True:
+            self.model.eval()
+
+            for i in range(0, len(self.data.test_list)):
+                test_x = self.data.test_list[i].to(self.model.device_id)
+                test_y = self.model.forward(test_x)
+                test_y = tr.argmax(test_y).data.cpu().numpy().flatten()[0]
+                self.test_text[i].setText('[%0.1f]' % test_y)
+
+            self.loop_x.append(self.loop_count)
+            self.loop_y.append(self.coss.data.cpu().numpy().flatten()[0])
+            self.loop_item.setData(self.loop_x, self.loop_y)
+
+            self.loop_count += 1
+            self.coss = 0
+
+    def update_paint(self):
+        test_x = tr.tensor(self.paint_img.image, dtype=tr.float32, device=self.model.device_id)
+        test_x = test_x.reshape(1, 1, XYBDeepLearn.ImageSize, XYBDeepLearn.ImageSize)
+        test_y = self.model.forward(test_x)
+        test_y = tr.argmax(test_y).data.cpu().numpy().flatten()[0]
+        self.paint_img_text.setText('[%0.1f]' % test_y)
 
     def stop(self):
         print('stop')
         self.time.stop()
+        self.time_paint.stop()
+
+        test_x = tr.tensor(self.paint_img.image, dtype=tr.float32, device=self.model.device_id)
+        test_x = test_x.reshape(1, 1, XYBDeepLearn.ImageSize, XYBDeepLearn.ImageSize)
+        test_y = self.model.forward(test_x)
+        test_y = tr.argmax(test_y).data.cpu().numpy().flatten()[0]
+        self.paint_img_text.setText('[%0.1f]' % test_y)
 
     def reset(self):
         self.init_deeplearn()
--- main.py
+++ main.py
@@ -8,9 +8,19 @@ import pyqtgraph as pyqt
 import pyqtgraph.examples
 import sys
 import numpy as np
+import XYBDeepLearn
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import QtWindow.MainWindow
+
+'''
+model = XYBDeepLearn.XYBDeepModel()
+data = tr.ones(1, 3, 6, 6, device=model.device_id)
+print(model.conv01.weight)
+res = model.forward(data)
+print(res.shape)
+print(res)
+'''
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
@@ -19,5 +29,3 @@ Ui.setupUi(MainWindow)
 pyqtgraph.examples.run()
 MainWindow.show()
 sys.exit(app.exec_())
-
-
